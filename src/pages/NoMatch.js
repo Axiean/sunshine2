e1d8a9fbb9b1e23ab28eedeaaca299143aa3a7a7
Axiean <axieans@gmail.com>
2021-09-11 23:35:04 +0430

    add pathname to nomatch

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function NoMatch() {
+  const location = useLocation();
   return (
     <div>
-      <h2>hello</h2>
+      <h2>{location.pathname}</h2>
     </div>
   );
 }
