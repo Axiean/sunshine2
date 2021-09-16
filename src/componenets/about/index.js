aad2468d3b4fa7e081bfcbe22b19f52fad5b4485
Axiean <axieans@gmail.com>
2021-09-16 13:05:05 +0430

    img size and info

--- src/componenets/about/index.js
+++ src/componenets/about/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Pro1 from "../../Images/ProfilePic/inline_image_preview.png";
+import Pro2 from "../../Images/ProfilePic/pro1.png";
+import Pro3 from "../../Images/ProfilePic/pro2.png";
 import { Button } from "../ButtonElement";
 import "./about.css";
 function About() {
@@ -9,10 +11,10 @@ function About() {
       <h2 id="about-title">Our Community</h2>
       <div id="pro-box">
         <div id="pro-card">
-          <img src={Pro1} alt="Howard" />
+          <img src={Pro2} alt="Howard" />
           <div id="pro-text-info">
-            <h2>Esther Howard</h2>
-            <h5>Software Engineer</h5>
+            <h2>Nesilla Amiga</h2>
+            <h5>Product Designer</h5>
             <p>lorem ipsum o in dastanaaa</p>
           </div>
         </div>
@@ -25,7 +27,7 @@ function About() {
           </div>
         </div>
         <div id="pro-card">
-          <img src={Pro1} alt="Howard" />
+          <img src={Pro3} alt="Howard" />
           <div id="pro-text-info">
             <h2>Esther Howard</h2>
             <h5>Software Engineer</h5>
